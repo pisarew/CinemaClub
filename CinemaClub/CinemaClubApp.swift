c5b7306f9ac9bebf94c427e64626147b638a3ff7
pisarew <gapisarev@edu.hse.ru>
2024-05-01 16:17:15 +0300

    init xcodegen proj

---
+++ CinemaClub/CinemaClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaClubApp.swift
+//  CinemaClub
+//
+//  Created by Глеб Писарев on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CinemaClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
