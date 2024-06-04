221193151b85dde4f7b2323cd2f0685ab0606958
Мария Степанчук <mshq@MacBook-Air-Maria.local>
2024-06-04 22:01:20 +0300

    new

---
+++ CinemaClub/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  CinemaClub
+//
+//  Created by Мария Степанчук on 04.06.2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
+    
